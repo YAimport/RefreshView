84972e59a766b23a78f1608b85d01ccf44b51c37
yaa <yaagff@sina.cn>
2016-11-05 11:50:42 +0800

    fix

--- RefreshDemo/AppDelegate.swift
+++ RefreshDemo/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+//test
     fileprivate func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable: Any]?) -> Bool {
         return true
     }
